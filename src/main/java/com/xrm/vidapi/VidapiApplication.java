e53a422592fc4b1a7e0c5e309bf26969d3894b18
XavierRM <xavier.rodriguez.munoz@gmail.com>
2025-02-28 19:10:44 +0100

    Project setup

---
+++ src/main/java/com/xrm/vidapi/VidapiApplication.java
@@ -0,0 +1,13 @@
+package com.xrm.vidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xrm/vidapi/VidapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xrm.vidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
